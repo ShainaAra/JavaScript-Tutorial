1cf16f7f4433a95ec7e03bf52223575d76bba660
ShainaAra <shainaqueturas@gmail.com>
2025-09-25 17:08:54 +1000

    Lesson 11

--- Lesson 11/Todo List Final/11-todo-list Final.js
+++ Lesson 11/Todo List Final/11-todo-list Final.js
@@ -1,46 +1,54 @@
-//create array to store todos
 const todoList = [{
     name: 'make dinner',
-    dueDate: '2022-12-22'
+    dueDate: '2025-09-25'
 }, {
     name: 'wash dishes',
-    dueDate: '2022-12-22'
-}]; //no values inside called empty array
+    dueDate: '2025-09-25'
+}]; 
 
 renderTodoList();
 
 function renderTodoList() {
     let todoListHTML = '';
-
-    //generating the HTML
+   
     for (let i = 0; i < todoList.length; i++) {
-        const todoObject = todoList[i];
+        const todoObject = todoList[i]; 
+
         //const name = todoObject.name;
         //const dueDate = todoObject.dueDate;
         const { name, dueDate } = todoObject; //shortcut
         
+        //generating the HTML
         const html = `
-            <p>
-                ${name} ${dueDate}
-                <button onclick="
-                    todoList.splice(${i}, 1);
-                    renderTodoList();
-                ">Delete</button>
-            </p>
+            <div>${name}</div> 
+            <div>${dueDate}</div>
+            <button onclick="
+                todoList.splice(${i}, 1);
+                renderTodoList();
+            " class="delete-todo-button">Delete</button>      
         `;
         todoListHTML += html; // combine html together
     }
-    console.log(todoListHTML);
+
     document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
 
 function addTodo () {
     const inputElement = document.querySelector('.js-name-input');
 
-    const name = inputElement.value;  //value prperty represents the text in the textbox
+    const name = inputElement.value;  
 
-    todoList.push(name);
-    console.log(todoList); 
+    const dateInputElement = document.querySelector(' js-due-date-input'); 
+    
+    const dueDate = dateInputElement.value; 
+
+    todoList.push({
+        //name: name,
+        //dueDate: dueDate
+        //shorthand property syntax, if the property and the variable name are the same
+        name,
+        dueDate
+    });
 
     //reset the text in the text box
     inputElement.value = '';
