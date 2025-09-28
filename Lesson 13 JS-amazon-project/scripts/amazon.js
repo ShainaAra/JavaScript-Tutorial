014015ebad9e0493702afc7a472eecb8a1a17772
ShainaAra <shainaqueturas@gmail.com>
2025-09-28 14:26:55 +1000

    quantity selector

--- Lesson 13 JS-amazon-project/scripts/amazon.js
+++ Lesson 13 JS-amazon-project/scripts/amazon.js
@@ -26,7 +26,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -64,19 +64,26 @@ document.querySelectorAll('.js-add-to-cart')
 
             let matchingItem;
 
-
             cart.forEach((item) => {
                 if (productId === item.productId) {
                     matchingItem = item; // if a product is already in the cart
                 }
             });
+            //13c
+            const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`
+            );
+            
+            //const quantity = quantitySelector.value; //13d
+            const quantity = Number(quantitySelector.value); ///e
 
             if (matchingItem) {
-                matchingItem.quantity += 1;
+                //matchingItem.quantity += 1;
+                matchingItem.quantity += quantity; //e
             } else {
                 cart.push({
                     productId: productId,
-                    quantity: 1
+                    //quantity: 1
+                    quantity : quantity //e
             });
             }
             //we need a varaible to store teh total quantity
